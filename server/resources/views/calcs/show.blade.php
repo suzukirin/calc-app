6f39599530ed6de13e128454951a000660162ac6
suzukirin <qilin.liberty124@gmail.com>
2020-10-23 18:30:50 +0900

    なんか変

---
+++ server/resources/views/calcs/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CalcsApp</title>
+</head>
+
+<body>
+  <h1>計算アプリ</h1>
+  <h2>{{$answer}}</h2>
+
+  
+  
+  
+  
+  
+</body>
+
+</html>
---
+++ server/storage/framework/views/80240f52b86cdbcd1abcf2c6749009a69b991523.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CalcsApp</title>
+</head>
+
+<body>
+  <h1>計算アプリ</h1>
+  <h2><?php echo e($answer); ?></h2>
+
+  
+  
+  
+  
+  
+</body>
+
+</html><?php /**PATH /var/www/resources/views/calcs/show.blade.php ENDPATH**/ ?>
